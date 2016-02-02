01786ac265f32e004fc357c87bbd0959120886b7
jblorenzo <kzer95@gmail.com>
2016-02-02 17:36:50 +0800

    Initial commit

---
+++ app/src/main/java/com/bennegeek/jbnativeads/JBNativeAds.java
@@ -0,0 +1,175 @@
+package com.bennegeek.olxnativeads;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.NativeAppInstallAd;
+import com.google.android.gms.ads.formats.NativeContentAd;
+
+import java.util.ArrayList;
+
+/**
+ * Created by JB on 2/2/16.
+ *
+ * Simple loader for AdMob Native Ads
+ * Handles prefetching and invalidating old prefetched items
+ */
+
+interface JBNativeAdListener {
+    void didGetAd(NativeAd ad);
+}
+
+public class JBNativeAds {
+    public static final long AdExpirationMillis = 60 * 60 * 1000; // 1 hour
+    public int maximumAdsCached = 4;
+
+    private
+    @NonNull
+    String mUnitId;
+    private
+    @Nullable
+    Context mContext;
+
+    AdLoader.Builder mLoaderBuilder;
+
+    JBAdsCache mCache = new JBAdsCache(maximumAdsCached);
+
+    // Gets a cached ad that has not expired yet
+    public void getAd(final JBNativeAdListener listener) {
+        NativeAd ad = mCache.pop();
+        if (ad != null) {
+            listener.didGetAd(ad);
+
+            fillCache();
+        } else {
+            loadAd(new JBNativeAdsLoaderListener() {
+                @Override
+                public void didLoadAd(NativeAd ad) {
+                    getAd(listener);
+                }
+            });
+        }
+    }
+
+    public JBNativeAds(Context context, String unitId) {
+        mContext = context;
+        mUnitId = unitId;
+
+        mLoaderBuilder = new AdLoader.Builder(mContext, mUnitId)
+                .withAdListener(new AdListener() {
+                    @Override
+                    public void onAdFailedToLoad(int errorCode) {
+                        // Handle the failure by logging, altering the UI, etc.
+                    }
+                })
+                .withNativeAdOptions(new NativeAdOptions.Builder()
+                        // Methods in the NativeAdOptions.Builder class can be
+                        // used here to specify individual options settings.
+                        .build());
+
+        fillCache();
+    }
+
+    // Private Methods
+
+    private interface JBNativeAdsLoaderListener {
+        void didLoadAd(NativeAd ad);
+    }
+
+    private class JBAdsCache {
+        public int maximumAdsCached;
+
+        ArrayList<NativeAdWithTimestamp> mAdsCache = new ArrayList<NativeAdWithTimestamp>();
+
+        JBAdsCache(int max) {
+            maximumAdsCached = max;
+        }
+
+        public void add(NativeAd ad) {
+            synchronized (mAdsCache) {
+                mAdsCache.add(new NativeAdWithTimestamp(ad, System.currentTimeMillis()));
+            }
+        }
+
+        public NativeAd pop() {
+            synchronized (mAdsCache) {
+                if (mAdsCache.size() > 0) {
+                    NativeAdWithTimestamp ad = mAdsCache.get(0);
+
+                    mAdsCache.remove(0);
+
+                    long current = System.currentTimeMillis();
+                    if (current - ad.timestamp >= AdExpirationMillis) {
+                        return null;
+                    }
+
+                    return ad.ad;
+                }
+
+                return null;
+            }
+        }
+
+        public int size() {
+            synchronized (mAdsCache) {
+                return mAdsCache.size();
+            }
+        }
+
+    }
+
+    private class NativeAdWithTimestamp {
+        NativeAd ad;
+        long timestamp;
+
+        NativeAdWithTimestamp(NativeAd a, long t) {
+            ad = a;
+            timestamp = t;
+        }
+    }
+
+    private void cacheNativeAd(NativeAd ad) {
+        mCache.add(ad);
+    }
+
+    private void fillCache() {
+        int cacheSize = mCache.size();
+
+        if (cacheSize < maximumAdsCached) {
+            loadAd(null);
+        }
+    }
+
+    private void loadAd(final JBNativeAdsLoaderListener listener) {
+        if (listener != null) {
+            mLoaderBuilder
+                    .forAppInstallAd(new NativeAppInstallAd.OnAppInstallAdLoadedListener() {
+                        @Override
+                        public void onAppInstallAdLoaded(NativeAppInstallAd appInstallAd) {
+                            // Show the app install ad.
+                            cacheNativeAd(appInstallAd);
+                            listener.didLoadAd(appInstallAd);
+
+                            fillCache();
+                        }
+                    })
+                    .forContentAd(new NativeContentAd.OnContentAdLoadedListener() {
+                        @Override
+                        public void onContentAdLoaded(NativeContentAd contentAd) {
+                            // Show the content ad.
+                            cacheNativeAd(contentAd);
+                            listener.didLoadAd(contentAd);
+
+                            fillCache();
+                        }
+                    });
+        }
+        mLoaderBuilder.build().loadAd(new AdRequest.Builder().build());
+    }
+}
---
+++ app/src/main/java/com/bennegeek/jbnativeads/MainActivity.java
@@ -0,0 +1,68 @@
+package com.bennegeek.olxnativeads;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdLoader;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.formats.NativeAd;
+import com.google.android.gms.ads.formats.NativeAdOptions;
+import com.google.android.gms.ads.formats.NativeAppInstallAd;
+import com.google.android.gms.ads.formats.NativeContentAd;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        new JBNativeAds(this,"ca-app-pub-3940256099942544/2247696110");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
